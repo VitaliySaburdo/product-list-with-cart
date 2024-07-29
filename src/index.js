cda35824ef8b91304f3fe6b9b7a56e2079b698c2
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-07-29 11:21:00 +0300

    adaptive screen

--- src/index.js
+++ src/index.js
@@ -8,13 +8,22 @@ function renderList(cards) {
   const markup = cards
     .map((card, idx) => {
       return ` 
-      <li class="product-list__item"  id=${idx + 1}>
+      <li class="product-list__item"  id=${idx}>
         <img
           class="product-list__image"
+                        srcset="
+              ${card.image.desktop} 250w,
+              ${card.image.mobile} 325w
+              "
+              sizes="
+                (min-width: 1440px) 250px,
+                100vw"
           src=${card.image.mobile}
           alt=${card.name}
+              width="250"
+              height="210"
         />
-        <button class="product-list__button" data=${idx + 1}>
+        <button class="product-list__button" data=${idx}>
           <svg width="18" height="18">
             <use href="../assets/images/sprite.svg#icon-icon-add-to-cart"></use>
           </svg>
