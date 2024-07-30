fa1df9c10db5c1725019f1e19c9209a47abf51f2
VitaliySaburdo <vsaburdo@gmail.com>
2024-07-30 19:41:48 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -1,81 +1,58 @@
 import data from './data/data.json';
+import { renderList } from './js/renderList';
 
 const list = document.querySelector('.product-list');
 
 renderList(data);
 
-function renderList(cards) {
-  const markup = cards
-    .map((card, idx) => {
-      return `
-      <li class="product-list__item"  id=${idx}>
-        <img
-          class="product-list__image"
-                        srcset="
-              ${card.image.desktop} 250w,
-              ${card.image.mobile} 325w
-              "
-              sizes="
-                (min-width: 1440px) 250px,
-                100vw"
-          src=${card.image.mobile}
-          alt=${card.name}
-              width="250"
-              height="210"
-        />
-        <button class="product-list__button" data-id=${idx}>
-          <svg width="18" height="18">
-            <use href="../assets/images/sprite.svg#icon-icon-add-to-cart"></use>
-          </svg>
-          Add to Cart
-        </button>
-        <p class="product-list__category">${card.category}</p>
-        <p class="product-list__name">${card.name}</p>
-        <p class="product-list__price">$${card.price.toFixed(2)}</p>
-      </li>`;
-    })
-    .join('');
-
-  list.innerHTML = markup;
-}
-
 list.addEventListener('click', onBtn);
+const quantities = {};
 
 function onBtn(event) {
   const target = event.target;
-  let quantity = 0;
 
   if (target.closest('.product-list__button')) {
     const button = target.closest('.product-list__button');
+    const cardId = button.dataset.id;
+
+    // Initialize quantity if not already set
+    if (!quantities[cardId]) {
+      quantities[cardId] = 1;
+    }
+
     button.classList.add('selected');
     button.innerHTML = ` 
-              <button
-                class="product__button product__button--decrement"
-                aria-label="Decrease quantity"
-              >
-                <span class="btn__icon">-</span>
-              </button>
-              <span class="product__quantity">${quantity}</span>
-              <button
-                class="product__button product__button--increment"
-                aria-label="Increase quantity"
-              >
-                <span class="btn__icon">+</span>
-              </button>
+      <button
+        class="product__button product__button--decrement"
+        aria-label="Decrease quantity"
+        data-id=${cardId}
+      >
+        <span class="btn__icon">-</span>
+      </button>
+      <span class="product__quantity" data-id=${cardId}>${quantities[cardId]}</span>
+      <button
+        class="product__button product__button--increment"
+        aria-label="Increase quantity"
+        data-id=${cardId}
+      >
+        <span class="btn__icon">+</span>
+      </button>
     `;
 
-    const decrement = document.querySelector('.product__button--decrement');
-    const increment = document.querySelector('.product__button--increment');
+    const decrement = button.querySelector('.product__button--decrement');
+    const increment = button.querySelector('.product__button--increment');
     const quantityDisplay = button.querySelector('.product__quantity');
 
     decrement.addEventListener('click', () => {
-      quantity -= 1;
-      quantityDisplay.textContent = quantity;
+      if (quantities[cardId] > 0) {
+        quantities[cardId] -= 1;
+        quantityDisplay.textContent = quantities[cardId];
+      }
     });
+
     increment.addEventListener('click', () => {
-      quantity += 1;
-      quantityDisplay.textContent = quantity;
+      quantities[cardId] += 1;
+      quantityDisplay.textContent = quantities[cardId];
     });
-    console.log(quantity);
   }
 }
---
+++ src/js/renderList.js
@@ -0,0 +1,36 @@
+const list = document.querySelector('.product-list');
+
+export function renderList(cards) {
+  const markup = cards
+    .map((card, idx) => {
+      return `
+      <li class="product-list__item" id=${idx}>
+        <img
+          class="product-list__image"
+          srcset="
+            ${card.image.desktop} 250w,
+            ${card.image.mobile} 325w
+          "
+          sizes="
+            (min-width: 1440px) 250px,
+            100vw"
+          src=${card.image.mobile}
+          alt="${card.name}"
+          width="250"
+          height="210"
+        />
+        <button class="product-list__button" data-id=${idx}>
+          <svg width="18" height="18">
+            <use href="../assets/images/sprite.svg#icon-icon-add-to-cart"></use>
+          </svg>
+          Add to Cart
+        </button>
+        <p class="product-list__category">${card.category}</p>
+        <p class="product-list__name">${card.name}</p>
+        <p class="product-list__price">$${card.price.toFixed(2)}</p>
+      </li>`;
+    })
+    .join('');
+
+  list.innerHTML = markup;
+}
