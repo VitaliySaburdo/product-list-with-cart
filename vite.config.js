9edc3a1433c519982652846b4c5a151f783b3560
VitaliySaburdo <vsaburdo@gmail.com>
2024-07-31 20:59:05 +0300

    add

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,8 @@
 import { defineConfig } from 'vite';
-import posthtml from 'posthtml';
-import include from 'posthtml-include';
-import { resolve } from 'path';
-
-const posthtmlPlugin = () => {
-  return {
-    name: 'vite-plugin-posthtml',
-    transformIndexHtml(html) {
-      return posthtml([include({ root: resolve(__dirname, 'src') })])
-        .process(html)
-        .then(result => result.html);
-    },
-  };
-};
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
-  },
-  plugins: [posthtmlPlugin()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
   },
 });
