1c16b04407fbfc3861203de7507d36701dbe2c15
VitaliySaburdo <vsaburdo@gmail.com>
2024-08-02 18:14:44 +0300

    modal-window

--- src/js/renderTotal.js
+++ src/js/renderTotal.js
@@ -10,6 +10,6 @@ export function renderTotal(total) {
           </svg>
           <p class="cart__delivery--text">This is a <b>carbon-neutral</b> delivery</p>
         </div>
-        <button class="cart__total--btn">Confirm Order</button>
+        <button class="btn">Confirm Order</button>
   `;
 }
